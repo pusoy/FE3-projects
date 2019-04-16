4eb5648cdae6b9e45166878bb66bb9e0d247b33d
japusoy <49558772+japusoy@users.noreply.github.com>
2019-04-16 10:25:50 +0800

    Update pusoy.js

--- js/pusoy.js
+++ js/pusoy.js
@@ -1,252 +1,69 @@
-(function() {
-  var pusoy = (function() {
-    var pusoy = function(selector) {
-      // Allows calling of pusoy without the 'new' keyword
-      if (this instanceof pusoy) {
-        if(typeof selector == 'object') {
-          // Constructor was passed a HTML Node,
-          // Manually assign this.el as an array containing the single Node,
-          // so as to emulate the results of document.querySelectorAll()
-          this.el = [selector];
-          return this;
-        } else {
-          this.el = document.querySelectorAll(selector);
-        }
-      } else {
-        return new pusoy(selector);
-      }
-    };
 
-    pusoy.prototype.css = function(styles) {
-      for(var i=0;i<this.el.length;i++) {
-        for (var property in styles) {
-          this.el[i].style[property] = styles[property];
-        }
-      }
-    };
+pusoy = (function (document, window, pusoy) {
+  // Node covers all elements, but also the document objects
+  var node = Node.prototype,
+      nodeList = NodeList.prototype,
+      forEach = 'forEach',
+      trigger = 'trigger',
+      each = [][forEach],
+      // note: createElement requires a string in Firefox
+      dummy = document.createElement('i');
 
-    pusoy.prototype.setClass = function(cl) {
-      for(var i=0;i<this.el.length;i++) {
-        this.el[i].className = cl;
-      }
-    };
+  nodeList[forEach] = each;
 
-    pusoy.prototype.appendClass = function(cl) {
-      for(var i=0;i<this.el.length;i++) {
-        this.el[i].className += ' ' + cl;
-      }
-    };
+  // we have to explicitly add a window.on as it's not included
+  // in the Node object.
+  window.on = node.on = function (event, fn) {
+    this.addEventListener(event, fn, false);
 
-    pusoy.prototype.hasClass = function(cl) {
-      for(var i=0;i<this.el.length;i++) {
-        if (this.el[i].className.indexOf(cl) == -1) {
-          return false; // Immediately return, no class found in the set of matched objects.
-        }
-      }
-      return true; // Made it through the loop above without returning, all objects have said class
-    };
+    // allow for chaining
+    return this;
+  };
 
-    pusoy.prototype.classNames = function() {
-      classes = [];
-      for(var i=0;i<this.el.length;i++) {
-        classes.push(this.el[i].className);
-      }
-      return classes;
-    };
+  nodeList.on = function (event, fn) {
+    this[forEach](function (el) {
+      el.on(event, fn);
+    });
+    return this;
+  };
 
-    pusoy.prototype.removeClass = function(cl) {
-      for (var i = 0; i < this.el.length; i++) {
-        var classes = this.el[i].className.split(' ');
-        if (classes.indexOf(cl) != -1) {
-          classes.splice(classes.indexOf(cl), 1);
-          this.el[i].className = classes.join(' ');
-        }
-      }
-    };
+  // we save a few bytes (but none really in compression)
+  // by using [trigger] - really it's for consistency in the
+  // source code.
+  window[trigger] = node[trigger] = function (type, data) {
+    // construct an HTML event. This could have
+    // been a real custom event
+    var event = document.createEvent('HTMLEvents');
+    event.initEvent(type, true, true);
+    event.data = data || {};
+    event.eventName = type;
+    event.target = this;
+    this.dispatchEvent(event);
+    return this;
+  };
 
-    pusoy.prototype.on = function(event, callback) {
-      for(var i=0;i<this.el.length;i++) {
-        this.el[i].addEventListener(event, callback);
-      }
-    };
+  nodeList[trigger] = function (event) {
+    this[forEach](function (el) {
+      el[trigger](event);
+    });
+    return this;
+  };
 
-    pusoy.prototype.attr = function(attributes) {
-      // Getter
-      if (typeof attributes == 'string') {
-        for(var i=0;i<this.el.length;i++) {
-          return this.el[i][attributes];
-        }
-      }
-      // Setter
-      for (var attribute in attributes) {
-        for(var i=0;i<this.el.length;i++) {
-          this.el[i][attribute] = attributes[attribute];
-        }
-      }
-    };
+  pusoy = function (s) {
+    // querySelectorAll requires a string with a length
+    // otherwise it throws an exception
+    var r = document.querySelectorAll(s || '☺'),
+        length = r.length;
+    // if we have a single element, just return that.
+    // if there's no matched elements, return a nodeList to chain from
+    // else return the NodeList collection from qSA
+    return length == 1 ? r[0] : r;
+  };
 
-    pusoy.prototype.text = function(text) {
-      var currentText = [];
-      for(var i=0;i<this.el.length;i++) {
-        // if text is undefined then we are calling as a 'getter' so
-        // lets store an array of all of the elements text and return it to
-        // the user
-        if (typeof text == 'undefined') {
-          currentText.push(this.el[i].innerHTML);
-        } else {
-          this.el[i].innerHTML = text;
-        }
-      }
+  // pusoy.on and pusoy.trigger allow for pub/sub type global
+  // custom events.
+  pusoy.on = node.on.bind(dummy);
+  pusoy[trigger] = node[trigger].bind(dummy);
 
-      // Return just the first result if there is only one
-      if (currentText.length == 1) {
-        return currentText[0];
-      } else if (currentText.length > 1) {
-        return currentText;
-      }
-    };
-
-    pusoy.prototype.each = function(callback) {
-      for (var i = 0; i < this.el.length; i++) {
-        // How to return instance of pusoy element???
-        callback(i, new pusoy(this.el[i]));
-      }
-    };
-
-    pusoy.ajax = function(options) {
-      return new Promise(function(resolve, reject) {
-        var http = new XMLHttpRequest();
-
-        http.open(options.method.toUpperCase(), options.url, true);
-
-        http.onreadystatechange = function() {
-          if (http.readyState == XMLHttpRequest.DONE) {
-            if (http.status == 200) {
-              resolve(JSON.parse(http.responseText));
-            } else {
-              reject(Error('Server responded with a status of: ' + http.status));
-            }
-          }
-        };
-
-        // So we're not sending an undefined dataset to the server
-        options.data = options.data || '';
-
-        // If the request type is post
-        // set header to default data type of json
-        if (options.method.toUpperCase() == 'POST') {
-          http.setRequestHeader('Content-type', 'application/json');
-          options.data = JSON.stringify(options.data);
-        }
-
-        // Set all headers
-        if (options.headers) {
-          pusoy.each(options.headers, function(key, value) {
-            http.setRequestHeader(key, value);
-          });
-        }
-
-        http.send(options.data);
-      });
-    };
-
-    pusoy.get = function(url) {
-      return pusoy.ajax({
-        method: 'GET',
-        url: url
-      });
-    };
-
-    pusoy.post = function(url, data, headers) {
-      return pusoy.ajax({
-        method: 'POST',
-        url: url,
-        data: data,
-        headers: headers
-      });
-    };
-
-    pusoy.each = function(collection, callback) {
-      if (typeof collection == 'array') {
-        for (var i = 0; i < collection.length; i++) {
-          callback(i, collection[i]);
-        }
-      }
-
-      if (typeof collection == 'object') {
-        for (var key in collection) {
-          callback(key, collection[key]);
-        }
-      }
-    };
-
-    pusoy.on = function( elem, types, selector, data, fn, one ) {
-      var origFn, type;
-
-      // Types can be a map of types/handlers
-      if ( typeof types === "object" ) {
-
-        // ( types-Object, selector, data )
-        if ( typeof selector !== "string" ) {
-
-          // ( types-Object, data )
-          data = data || selector;
-          selector = undefined;
-        }
-        for ( type in types ) {
-          on( elem, type, selector, data, types[ type ], one );
-        }
-        return elem;
-      }
-
-      if ( data == null && fn == null ) {
-
-        // ( types, fn )
-        fn = selector;
-        data = selector = undefined;
-      } else if ( fn == null ) {
-        if ( typeof selector === "string" ) {
-
-          // ( types, selector, fn )
-          fn = data;
-          data = undefined;
-        } else {
-
-          // ( types, data, fn )
-          fn = data;
-          data = selector;
-          selector = undefined;
-        }
-      }
-      if ( fn === false ) {
-        fn = returnFalse;
-      } else if ( !fn ) {
-        return elem;
-      } 
-};
-
-
-
-
-    pusoy.getClass = function(elem) {
-      return elem.getAttribute && elem.getAttribute( "class" ) || "";
-    }; 
-    pusoy.classesToArray = function(value) {
-      if ( Array.isArray( value ) ) {
-        return value;
-      }
-      if ( typeof value === "string" ) {
-        return value.match( rnothtmlwhite ) || [];
-      }
-      return [];
-    };  
-
-    return pusoy;
-  })();
-
-  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = pusoy;
-  } else {
-    window.pusoy = pusoy;
-  }
-})();
+  return pusoy;
+})(document, this);
